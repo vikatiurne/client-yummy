5aa259ff72d6ba2735365cfcd43883cb3b711489
vikatiurne <viktoriya83-08@ukr.net>
2023-11-23 09:59:45 +0200

    username

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,6 +18,7 @@ const Header = () => {
   const price = useSelector((state) => state.basket.totalPrice);
   const orders = useSelector((state) => state.basket.order);
   const userId = useSelector((state) => state.auth.user.id);
+  console.log(userName)
 
   const dispatch = useDispatch();
 
