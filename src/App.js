ac09eab24e815174f80672088f2b38551a0157c0
vikatiurne <viktoriya83-08@ukr.net>
2023-11-29 10:02:32 +0200

    router

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
+        <Route path="/*" element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="basket" element={<Basket />} />
           <Route path="checkout" element={<Checkout />} />
